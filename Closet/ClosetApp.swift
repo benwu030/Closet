524fda81142f46c964f6927fad44ecc941955074
Ben Wu <benwu013@connect.hku.hk>
2024-01-31 12:51:42 +0800

    Initial Commit

---
+++ Closet/ClosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosetApp.swift
+//  Closet
+//
+//  Created by Ben Wu on 31/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct ClosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
